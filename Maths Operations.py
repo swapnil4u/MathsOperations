edea082ffb39839c6eb9589f2224babd985cb93f
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-01-31 18:58:12 +0530

    Maths Operations
    Maths Operations In Python

---
+++ Maths Operations.py
@@ -0,0 +1,33 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>  print("Gayathri")
+SyntaxError: unexpected indent
+>>> print("Gayathri")
+Gayathri
+>>> 2+3
+5
+>>> 10-5
+5
+>>> 2*2
+4
+>>> 25/5
+5.0
+>>> 25%2
+1
+>>> 10/3
+3.3333333333333335
+>>> 10//3
+3
+>>> 4.563/2.36
+1.9334745762711865
+>>> 4.563//2.36
+1.0
+>>> 2*2*2
+8
+>>> 2**3
+8
+>>> 5**8
+390625
+>>> print("udfh00")
+
+
